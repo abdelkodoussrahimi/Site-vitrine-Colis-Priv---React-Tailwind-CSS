c334c8d323de4a02d1e32b7648d4cc9313617b2f
abdelkodoussrahimi <abdelkodoussr@gmail.com>
2025-11-05 16:22:51 +0100

    feat: update Banner responsiveness and README image

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -27,7 +27,7 @@ const Banner = () => {
           initial="hidden" 
           whileInView={'show'} 
           viewport={{once:false,amount:0.7}} 
-          className='mb-6 text-[36px]  lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+          className='mb-6 text-[36px]  lg:text-[60px] font-secondary font-semibold uppercase leading-[1] whitespace-nowrap'>
           <span className=' text-white mr-4'>Service de</span>
           <TypeAnimation
             sequence={[
